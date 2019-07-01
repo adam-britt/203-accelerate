dab913061b1b613ac7aaa29b08fc9851bacd1ddf
Adam Britt <adamrussellbritt@gmail.com>
2019-07-01 14:25:00 -0500

    custom post type function edits

--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -28,9 +28,7 @@ function create_custom_post_types() {
 			),
 			'public' => true,
 			'has_archive' => true,
-			'rewrite' => array(
-				'slug' => 'case-studies'
-			),
+			'rewrite' => array( 'slug' => 'case-studies' ),
 		),
 	);
 }
