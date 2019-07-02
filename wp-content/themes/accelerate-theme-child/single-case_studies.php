0e8e11fc03871853076f6eeb27983c73ba6c1031
Adam Britt <adamrussellbritt@gmail.com>
2019-07-02 13:17:36 -0500

    case study image attachment method change to ameliorate file size error

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -14,12 +14,48 @@
 
 get_header(); ?>
 
-	<div id="primary" class="site-content sidebar">
-		<div class="main-content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
+<div id="primary" class="site-content sidebar">
+	<div class="main-content" role="main">
+		<?php while ( have_posts() ) : the_post();
+			$services = get_field('services');
+			$client = get_field('client');
+			$link = get_field('site_link');
+			$image_1 = get_field('image_1');
+			$image_2 = get_field('image_2');
+			$image_3 = get_field('image_3');
+			$size = "full";
+		?>
+
+			
+		<article class="case-study">
+			<aside class="case-study-sidebar">
+				<h2><?php the_title() ?></h2>
+				<h5><?php echo $services; ?></h5>
+				<h6>Client: <?php echo $client; ?></h6>
+				
 				<?php the_content(); ?>
-			<?php endwhile; // end of the loop. ?>
-		</div><!-- .main-content -->
-	</div><!-- #primary -->
+				
+				<p><strong><a href="<?php echo $link; ?>">Site Link</a></strong></p>
+			</aside>
+
+			<div class="case-study-images">
+				<?php if($image_1) { ?>
+					<?php echo wp_get_attachment_image( $image_1, $size ); ?>
+					<img src="<?php echo $image_1; ?>" alt="" />
+				<?php } ?>
+				<?php if($image_2) { ?>
+					<?php echo wp_get_attachment_image( $image_2, $size ); ?>
+					<img src="<?php echo $image_2; ?>" alt="" />
+				<?php } ?>
+				<?php if($image_3) { ?>
+					<?php echo wp_get_attachment_image( $image_3, $size ); ?>
+					<img src="<?php echo $image_3; ?>" alt="" />
+				<?php } ?>
+			</div>
+		</article>
+
+		<?php endwhile; // end of the loop. ?>
+	</div><!-- .main-content -->
+</div><!-- #primary -->
 
 <?php get_footer(); ?>
